ed954547acb98b236d90d71f6f15d0ab49d84474
sahilkumardev <sahilkumar710@outlook.com>
2025-08-26 12:56:34 +0530

    feat: add button component with variants and utility functions
    - Implemented a new button component with customizable variants and sizes using class-variance-authority.
    - Added utility function `cn` for merging class names with Tailwind CSS.
    - Created a JSON registry for the button component and updated the main registry.
    - Established global styles with CSS variables for theming and dark mode support.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import { Button } from "@/registry/components/button";
 import React from "react";
 
 const Page = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1>Project Started</h1>
+    <div className="h-screen flex items-center justify-center flex-col gap-4">
+      <Button>Project Started</Button>
+
+      <Button>
+        <code>
+          pnpm dlx shadcn@latest add http://localhost:3000/r/button.json
+        </code>
+      </Button>
     </div>
   );
 };
