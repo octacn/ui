849614fe32613c91b3336828094e27c898324cf2
sahilkumardev <sahilkumar710@outlook.com>
2025-09-25 18:07:44 +0530

    feat: remove contact form and related components

--- app/(apps)/(routes)/blocks/page.tsx
+++ app/(apps)/(routes)/blocks/page.tsx
@@ -1,10 +1,9 @@
-import ProLibraryCta from "@/components/pro-library-cta";
 import React from "react";
 
 const Page = () => {
   return (
-    <div className="flex flex-col items-center gap-10 justify-center py-10 px-20">
-      <ProLibraryCta show />
+    <div className="py-10">
+       blocks
     </div>
   );
 };
