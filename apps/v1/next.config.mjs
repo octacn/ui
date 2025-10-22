c498a663f78546558424125f475a83e983308b9c
sahilkumardev <sahilkumar710@outlook.com>
2025-10-22 06:35:36 +0530

    Refactor: Replace TypeScript config file with ES module version
    - Deleted the TypeScript configuration file `next.config.ts`.
    - Added a new ES module configuration file `next.config.mjs` with the same settings.

--- apps/v1/next.config.mjs
+++ apps/v1/next.config.mjs
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next"
 import { createMDX } from "fumadocs-mdx/next"
 
 const withMDX = createMDX()
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   devIndicators: false,
   typescript: {
     ignoreBuildErrors: true,
@@ -23,6 +22,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
 }
 
 export default withMDX(nextConfig)
