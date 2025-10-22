ca49e6a261e37c971543938de40671e562f63d7a
sahilkumardev <sahilkumar710@outlook.com>
2025-10-22 05:55:13 +0530

    refactor: remove unused imports and update component to return static content

--- apps/v1/app/(apps)/(routes)/(hero)/example/blocks/page.tsx
+++ apps/v1/app/(apps)/(routes)/(hero)/example/blocks/page.tsx
@@ -1,8 +1,8 @@
-import React, { Suspense } from "react"
+import React from "react"
 
-import { BlockDisplay } from "@/components/block-display"
+// import { BlockDisplay } from "@/components/block-display"
 // import { Loading } from "@/registry/components/loading"
 
 export default function Page() {
-  return <BlockDisplay name="header-v1" />
+  return <div>hlo</div>
 }
